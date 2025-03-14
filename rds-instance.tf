a6dcdf4bbaff8839d5582009f70d8d7f3dae8683
Param Shah <shah.paramk@northeastern.edu>
2025-03-14 18:44:59 -0400

    Worked on s3 bucket

---
+++ rds-instance.tf
@@ -0,0 +1,25 @@
+resource "aws_db_instance" "csye6225_rds_instance" {
+  allocated_storage      = 20
+  storage_type           = var.storage_type
+  engine                 = var.engine
+  engine_version         = var.engine_version
+  instance_class         = var.instance_class
+  db_name                = var.db_name
+  identifier             = var.identifier
+  username               = var.db_user
+  password               = var.db_password
+  db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.name
+  parameter_group_name   = aws_db_parameter_group.csye6225_db_parameter_group.name
+  vpc_security_group_ids = [aws_security_group.db_security_group.id]
+  skip_final_snapshot    = true
+  multi_az               = false
+  publicly_accessible    = false
+  storage_encrypted      = true
+
+  tags = {
+    Name        = "csye6225"
+    Environment = "Development"
+  }
+
+
+}
---
+++ s3.tf
@@ -0,0 +1,100 @@
+# Generate a random UUID for the S3 bucket name
+resource "random_uuid" "bucket_name" {}
+
+# S3 Bucket with UUID as name
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket        = random_uuid.bucket_name.result # Assign the UUID as bucket name
+  force_destroy = true                           # Ensures Terraform can delete bucket even if not empty
+}
+
+# Enable Default Encryption for S3 Bucket
+resource "aws_s3_bucket_server_side_encryption_configuration" "s3_encryption" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_iam_role" "ec2_role" {
+  name = "IAMRoleForEc2"
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "ec2.amazonaws.com"
+        },
+        "Action" : "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# Policy for managing the S3 bucket
+resource "aws_iam_policy" "s3_bucket_policy" {
+  name        = "S3BucketPolicy"
+  description = "Policy for managing the S3 bucket"
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:DeleteObject",
+          "s3:ListBucket",
+          "s3:PutBucketEncryption",
+          "s3:PutLifecycleConfiguration"
+        ],
+        "Resource" : [
+          "arn:aws:s3:::${aws_s3_bucket.s3_bucket.id}",
+          "arn:aws:s3:::${aws_s3_bucket.s3_bucket.id}/*"
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "rds:DescribeDBInstances",
+          "rds:Connect"
+        ]
+        Resource = aws_db_instance.csye6225_rds_instance.arn
+      }
+    ]
+  })
+}
+
+
+
+# Attach the policy to the role
+resource "aws_iam_role_policy_attachment" "s3_policy_attachment" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.s3_bucket_policy.arn
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_private_bucket" {
+  bucket                  = aws_s3_bucket.s3_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# Lifecycle policy for transition to STANDARD_IA storage class
+resource "aws_s3_bucket_lifecycle_configuration" "private_bucket_lifecycle" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  rule {
+    id     = "TransitionRule"
+    status = "Enabled"
+
+    transition {
+      days          = 30
+      storage_class = "STANDARD_IA"
+    }
+  }
+}
