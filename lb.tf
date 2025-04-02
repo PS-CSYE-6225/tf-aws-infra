b1b0806ea5309c026322b6e3785c9fe548bdcfa5
Param Shah <shah.paramk@northeastern.edu>
2025-04-01 22:37:01 -0400

    created load balancer for the autoscaling

---
+++ lb.tf
@@ -0,0 +1,43 @@
+# Create Application Load Balancer
+resource "aws_lb" "app_lb" {
+  name               = "${var.network_name}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.load_balancer_sg.id]
+  subnets            = aws_subnet.public[*].id
+
+  tags = {
+    Name = "${var.network_name}-alb"
+  }
+}
+
+# Listener for Load Balancer
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+  
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_target_group.arn
+  }
+}
+
+# Target Group for Auto Scaling Group
+resource "aws_lb_target_group" "app_target_group" {
+  name     = "${var.network_name}-tg"
+  port     = var.app_port
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main[0].id
+
+  health_check {
+    enabled             = true
+    interval            = 30
+    path                = "/healthz"
+    timeout             = 10
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
+}
