660a9e2d77035294ee81c9c17b2b3e9be8357568
Param Shah <shah.paramk@northeastern.edu>
2025-04-01 22:40:44 -0400

    Updated load balancer

--- lb.tf
+++ lb.tf
@@ -16,7 +16,7 @@ resource "aws_lb_listener" "http_listener" {
   load_balancer_arn = aws_lb.app_lb.arn
   port              = 80
   protocol          = "HTTP"
-  
+
 
   default_action {
     type             = "forward"
